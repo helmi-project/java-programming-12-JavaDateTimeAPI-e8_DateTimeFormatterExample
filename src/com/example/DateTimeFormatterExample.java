1efbfbac0d3365bae1b9d3f551a09876f7d1a9bf
Helmi Hartanto <>
2021-01-07 14:09:07 +0700

    First Commit

---
+++ src/com/example/DateTimeFormatterExample.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+public class DateTimeFormatterExample {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    ZonedDateTime now = ZonedDateTime.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
+    System.out.println(now.format(formatter));
+    formatter = DateTimeFormatter.ISO_ORDINAL_DATE;
+    System.out.println(now.format(formatter));
+    formatter = DateTimeFormatter.ofPattern("EEEE, MMMM dd, yyyy G, hh:mm a 'Q'q VV");
+    System.out.println(now.format(formatter));
+    formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+    System.out.println(now.format(formatter));
+  }
+}
